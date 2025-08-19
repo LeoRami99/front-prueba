6a88ed72812ff8cc5d4f873c6f83aa8efca94ca6
Leonardo Ramírez <juanlov4321@gmail.com>
2025-08-19 17:09:53 -0500

    chore: update CardProduct component layout and styling for improved presentation

--- components/CardProduct.tsx
+++ components/CardProduct.tsx
@@ -1,11 +1,24 @@
-import { Image, Text, View } from "react-native";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import { Image, Pressable, Text, View } from "react-native";
 
 const CardProduct = () => {
 	return (
-		<View className='bg-white p-4 rounded-lg shadow-md'>
-			<Image source={{ uri: "https://via.placeholder.com/150" }} className='w-full h-32 rounded-lg mb-4' />
-			<Text className='text-lg font-semibold'>Product Name</Text>
-			<Text className='text-gray-500 mt-1'>$19.99</Text>
+		<View className='bg-white rounded-2xl h-[280px] border border-gray-200 min:w-1/2 relative'>
+			<Image
+				source={{ uri: "https://cdn.pixabay.com/photo/2025/03/13/10/50/fall-9467534_1280.jpg" }}
+				className='w-full h-full rounded-2xl absolute'
+			/>
+			<View className='p-4 min:h-[280px] rounded-2xl w-full h-full justify-between bg-black/50 hover:backdrop-blur-md'>
+				<View>
+					<Text className='text-2xl font-bold text-white'>Product Name</Text>
+					<Text className='text-gray-500 mt-1'>$19.99</Text>
+				</View>
+
+				<Pressable className='mt-4 bg-[#1bb1f7] p-2 rounded-2xl flex flex-row w-full justify-center items-center gap-2'>
+					<Text className='text-white text-center'>Show</Text>
+					<FontAwesome5 name='chevron-right' size={15} color='white' />
+				</Pressable>
+			</View>
 		</View>
 	);
 };
