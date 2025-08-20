24a5a61e9f8665a5c1fadcdb70d0f3be25988c20
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-08-19 23:49:04 -0500

    chore: implement pull-to-refresh functionality and update GridProducts integration

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,20 @@
-import CardProduct from "@/components/CardProduct";
 import CarouselComponent from "@/components/CarouselProducts";
-import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import GridProducts from "@/components/GridProducts";
 import { Stack } from "expo-router";
-import { Pressable, ScrollView, Text, TextInput, View } from "react-native";
+import { useCallback, useState, type ComponentType } from "react";
+import { RefreshControl, ScrollView, View } from "react-native";
 
 const ViewProducstMain = () => {
+	const [refreshing, setRefreshing] = useState(false);
+	const GridProductsTyped = GridProducts as unknown as ComponentType<{ isRefetchProducts: boolean }>;
+
+	const onRefresh = useCallback(() => {
+		setRefreshing(true);
+		setTimeout(() => {
+			setRefreshing(false);
+		}, 2000);
+	}, []);
+
 	return (
 		<View className='flex-1 bg-white '>
 			<Stack.Screen
@@ -14,36 +24,24 @@ const ViewProducstMain = () => {
 
 					title: "Products",
 					// andd button of cart
-					headerRight: () => (
-						<View className='flex-row items-center p-4'>
-							<Pressable>
-								<FontAwesome5 name='shopping-cart' size={24} color='black' />
-							</Pressable>
-						</View>
-					),
+					// headerRight: () => (
+					// 	<View className='flex-row items-center p-4'>
+					// 		<Pressable>
+					// 			<FontAwesome5 name='shopping-cart' size={24} color='black' />
+					// 		</Pressable>
+					// 	</View>
+					// ),
 				}}
 			/>
 			<View className='container p-2 space-y-4 flex mx-auto max-w-4xl'>
 				{/* <Text className='text-2xl'>Products Main View</Text> */}
-				<ScrollView style={{ marginTop: 10 }}>
+				<ScrollView
+					style={{ marginTop: 10 }}
+					refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} colors={["#9Bd35A", "#689F38"]} />}>
 					<CarouselComponent />
-					<TextInput className=' bg-white border p-4 m-2 rounded-full' placeholder='Buscar Producto' />
-					<View style={{ flexDirection: "row", flexWrap: "wrap", gap: 10, margin: 10 }}>
-						{Array(10)
-							.fill(0)
-							.map((_, index) => (
-								<View key={index} style={{ width: "48%", marginBottom: 10 }}>
-									<CardProduct />
-								</View>
-							))}
-					</View>
-					<View className='flex flex-row justify-center mt-4'>
-						<Text className='text-gray-500'>1</Text>
-						<Text className='text-gray-500 ml-2'>2</Text>
-						<Text className='text-gray-500 ml-2'>3</Text>
-						<Text className='text-gray-500 ml-2'>4</Text>
-						<Text className='text-gray-500 ml-2'>5</Text>
-					</View>
+					<GridProductsTyped
+						isRefetchProducts={refreshing} // Pass the refreshing state to GridProducts
+					/>
 				</ScrollView>
 			</View>
 		</View>
