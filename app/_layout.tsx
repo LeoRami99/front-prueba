b22a2273df09ac7d450bdccd89e0c10795ee5cb0
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-08-21 02:36:06 -0500

    feat: integrate Redux provider and add Toast component to RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,15 @@
+import { store } from "@/app/store";
 import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
+import { Provider } from "react-redux";
 import "../styles/global.css";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
+import Toast from "react-native-toast-message";
 
 export default function RootLayout() {
 	const colorScheme = useColorScheme();
@@ -28,17 +31,20 @@ export default function RootLayout() {
 
 	return (
 		<QueryClientProvider client={queryClient}>
-			<ThemeProvider value={colorScheme === "light" ? DarkTheme : DefaultTheme}>
-				<Stack
-					screenOptions={{
-						headerShadowVisible: false, // iOS
-						sheetElevation: 0, // Android
-					}}>
-					<Stack.Screen name='index' />
-					<Stack.Screen name='+not-found' />
-				</Stack>
-				<StatusBar style='auto' />
-			</ThemeProvider>
+			<Provider store={store}>
+				<ThemeProvider value={colorScheme === "light" ? DarkTheme : DefaultTheme}>
+					<Stack
+						screenOptions={{
+							headerShadowVisible: false, // iOS
+							sheetElevation: 0, // Android
+						}}>
+						<Stack.Screen name='index' />
+						<Stack.Screen name='+not-found' />
+					</Stack>
+					<Toast />
+					<StatusBar style='auto' />
+				</ThemeProvider>
+			</Provider>
 		</QueryClientProvider>
 	);
 }
