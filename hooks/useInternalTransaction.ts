5196fa0a8deb70175ca59cfc6d55b9e3f68a5ce8
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-08-21 02:37:24 -0500

    feat: add hook for retrieving internal transaction using query

---
+++ hooks/useInternalTransaction.ts
@@ -0,0 +1,9 @@
+import { getInternalTransaction } from "@/services/internal.transaction.service";
+import { useQuery } from "@tanstack/react-query";
+
+export const useGetInternalTransaction = (id: string) => {
+	return useQuery({
+		queryKey: ["internalTransaction", id],
+		queryFn: () => getInternalTransaction(id),
+	});
+};
