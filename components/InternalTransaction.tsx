29ce15f9016d2efc89298f49671beafc908d4250
Juan Leonardo Ramirez Velasquez <juanlov4321@gmail.com>
2025-08-21 22:47:15 -0500

    refactor: enhance InternalTransaction component with improved navigation and UI updates

--- components/InternalTransaction.tsx
+++ components/InternalTransaction.tsx
@@ -1,10 +1,16 @@
+import type { AppDispatch } from "@/app/store";
 import { selectInternalTx } from "@/features/selectors";
+import { resetSteps } from "@/features/steps/steps.slice";
 import { useGetInternalTransaction } from "@/hooks/useInternalTransaction";
+import { useRouter } from "expo-router";
 import React, { useCallback, useState } from "react";
 import { ActivityIndicator, Pressable, RefreshControl, ScrollView, Text, View } from "react-native";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const InternalTransaction = () => {
+	const dispatch = useDispatch<AppDispatch>();
+
+	const router = useRouter();
 	const internalTxId = useSelector(selectInternalTx);
 	const [isPulling, setIsPulling] = useState(false);
 	const { data: tx, isLoading, error, refetch } = useGetInternalTransaction(internalTxId);
@@ -19,7 +25,8 @@ const InternalTransaction = () => {
 	}, [refetch]);
 
 	const goHome = () => {
-		// router.replace("/");
+		dispatch(resetSteps());
+		router.replace("/");
 	};
 
 	const getStatusStyle = () => {
@@ -66,7 +73,7 @@ const InternalTransaction = () => {
 	return (
 		<View className='flex-1'>
 			<ScrollView refreshControl={<RefreshControl refreshing={isPulling} onRefresh={onRefresh} />}>
-				<View className='bg-white border border-slate-200 p-5 rounded-2xl'>
+				<View className=''>
 					<Text className='text-2xl font-bold mb-3'>Detalles de la transacción</Text>
 
 					<View className={`self-start rounded-full px-3 py-1 ${getStatusStyle()}`}>
@@ -90,10 +97,14 @@ const InternalTransaction = () => {
 							<Text className='text-green-700 font-semibold'>¡Pago aprobado!</Text>
 						</View>
 					)}
-
-					<Pressable onPress={() => refetch()} className='mt-4'>
-						<Text className='text-blue-600 text-lg'>Actualizar</Text>
-					</Pressable>
+					{tx.status === "approved" ?
+						<Pressable className='mt-5 bg-blue-400 p-4 rounded-xl' onPress={goHome}>
+							<Text className='text--700 font-semibold'>Go to Home</Text>
+						</Pressable>
+					:	<Pressable onPress={() => refetch()} className='mt-4'>
+							<Text className='bg-blue-400 text-center p-4 rounded-2xl text-lg'>Actualizar</Text>
+						</Pressable>
+					}
 				</View>
 			</ScrollView>
 		</View>
